68fcb747f6b0e3047de86280c33aa0f50539ff90
Dídac Ballabriga <didac@ballabriga.com>
2022-03-01 18:07:03 +0100

    Exercici3

--- dist/index.js
+++ dist/index.js
@@ -8,16 +8,46 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+let reportAcudits = [];
 const loadJokes = () => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
     try {
         const response = yield fetch('https://icanhazdadjoke.com/slack');
         const data = yield response.json();
+        showDom(data);
         console.log(data.attachments[0].text);
-        (_a = document.getElementById("joke")) === null || _a === void 0 ? void 0 : _a.innerHTML = data.attachments[0].text;
     }
     catch (error) {
         console.log(error);
     }
 });
+const dayMonthYear = new Date();
+let date = dayMonthYear.toISOString();
+var rang = "0";
+function loadScore(score) {
+    if (score === 1) {
+        rang = "1";
+    }
+    else if (score === 2) {
+        rang = "2";
+    }
+    else {
+        rang = "3";
+    }
+    loadJokes();
+    return rang;
+}
+function showDom(data) {
+    var _a;
+    let newJoke = new Joke(data.attachments[0].text, rang, date);
+    reportAcudits.push(newJoke);
+    return (_a = document.getElementById("joke")) === null || _a === void 0 ? void 0 : _a.innerHTML = data.attachments[0].text;
+}
+class Joke {
+    constructor(joke, score, date) {
+        this.joke = joke;
+        this.score = score;
+        this.date = date;
+        return { joke, score, date };
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,47 @@
+let reportAcudits: string[] = [];
+
 const loadJokes = async () => {
     try{
         const response = await fetch('https://icanhazdadjoke.com/slack')
         const data = await response.json();
+        showDom(data)
         console.log(data.attachments[0].text);
-        document.getElementById("joke")?.innerHTML = data.attachments[0].text;
     } catch(error){
         console.log(error);
     }
 }
 
+const dayMonthYear = new Date();
+let date = dayMonthYear.toISOString();
 
+var rang:String ="0";
+function loadScore(score:number){
+    if(score===1){
+        rang = "1";
+    }else if(score===2){
+        rang = "2";
+    }else{
+        rang = "3";
+    }
+    loadJokes();
+    return rang;
+}
+
+function showDom(data){
+    let newJoke = new Joke(data.attachments[0].text,rang,date);
+    reportAcudits.push(newJoke)
+    return document.getElementById("joke")?.innerHTML = data.attachments[0].text;
+}
+
+class Joke{
+    joke: string;
+    score: string;
+    date: string;
+    constructor(joke:string,score:string,date:string){
+       this.joke = joke;
+       this.score = score;
+       this.date = date;
+       return {joke,score,date};
+    }
+}
 
